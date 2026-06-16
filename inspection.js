6fcf7fe94dcf96cb51f3f5ed7825204742c50325
WarwickBuildings <wbaftercare@outlook.com>
2026-06-16 10:20:35 +0100

    Update inspection.js

--- inspection.js
+++ inspection.js
@@ -1,7 +1,51 @@
-console.log("inspection page loaded");
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-app.js";
 
-window.saveInspection = function () {
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  serverTimestamp
+} from "https://www.gstatic.com/firebasejs/12.14.0/firebase-firestore.js";
 
-  alert("Save button works!");
+const firebaseConfig = {
+  apiKey: "AIzaSyDxzI-4C04LvHKj-g99pNr0UPiQuRs-RY0",
+  authDomain: "base-checks-8057f.firebaseapp.com",
+  projectId: "base-checks-8057f",
+  storageBucket: "base-checks-8057f.firebasestorage.app",
+  messagingSenderId: "1062442818469",
+  appId: "1:1062442818469:web:e0f47b4009d9ef0af1264c"
+};
 
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const params = new URLSearchParams(window.location.search);
+const jobId = params.get("id");
+
+window.saveInspection = async function () {
+
+  try {
+
+    await addDoc(collection(db, "inspections"), {
+
+      jobId: jobId,
+
+      baseLevel:
+        document.getElementById("baseLevel").checked,
+
+      notes:
+        document.getElementById("notes").value,
+
+      created:
+        serverTimestamp()
+    });
+
+    alert("Inspection saved successfully");
+
+  } catch (error) {
+
+    console.error(error);
+
+    alert("Failed to save inspection");
+  }
 };
